3e2eeb090122d07c1cc3c424f7c3b3fcddc4eb20
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-12-12 00:19:15 +0530

    updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -23,28 +23,56 @@ const Home = () => {
   return (
     <div className="home">
         <Navbar/>
-        <div className='home-cont'>
-            <div>
+        <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-heading">Master Your Future with AI Excellence</h1>
+        {/* <img
+        src="https://www.pngmart.com/files/21/Job-Interview-PNG-Picture.png"
+        alt="clothes that get you noticed"
+        className="home-mobile-img"
+      /> */}
+        <p className="home-description">
+          Aimock brings AI-driven mock interviews and skill-boosting tests for
+          every career path. Prepare for roles like software, GATE, and civil
+          services with tailored simulations. Gain confidence with expert
+          feedback and comprehensive mock tests. Upskill, stand out, and secure
+          your dream job!"
+        </p>
+        <p className="home-description"> 
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Eos veritatis cumque suscipit odio dolorum perspiciatis esse rem aliquid
+           non aspernatur sequi exercitationem nostrum maiores accusantium, 
+           officia maxime necessitatibus incidunt molestias illo modi aut commodi,
+            dignissimos ex unde? Praesentium, eos? Ducimus quis maiores quidem laborum
+             recusandae impedit perspiciatis molestias cum eum quae tempora sed, id sunt,
+              dolore eaque architecto. Vitae cumque maxime voluptas, nisi nostrum dignissimos! 
+              Facere quae, modi illum cumque ducimus corporis at eaque ullam possimus suscipit 
+              quam quos doloremque nobis reprehenderit voluptate deleniti earum, dolor asperiores qui non!
+               Consequatur vitae, consequuntur libero debitis impedit quo et eaque officiis alias.
+           </p>
+           </div>
+           <div className='hom-img'>
+           <img
+        src="https://www.pngmart.com/files/21/Job-Interview-PNG-Picture.png"
+        alt="clothes that get you noticed"
+        className="home-desktop-img"
+      />
+           </div>
+           </div>
+        <div class="buttonSet">
+          <div >
             <img src="./mock4[1].jpg" alt="" />
-            </div>
-            <div className="hom-main">
-                <h3>notifications</h3>
-                <article>
-
-                </article>
-            </div>
-            <div>
+            
+            <input type="button" className="shop-now-button" value="Mock Interview" onClick={goToMockInt} />
+          </div>
+          <div>
                 <img src="./mock7.jpg" alt="" />
+            
+            <input type="button" className="shop-now-button" value="Mock Tests" onClick={goToMockTest} />
             </div>
         </div>
-        <div className="home-mock">
-            <div className="mock-int">
-                <input type="button" value="Mock Interview" onClick={goToMockInt} />
-            </div>
-            <div className="mock-int">
-                <input type="button" value="Mock Tests" onClick={goToMockTest} />
-            </div>
-        </div>
+    
+        
         <Footer />
     </div>
   )
