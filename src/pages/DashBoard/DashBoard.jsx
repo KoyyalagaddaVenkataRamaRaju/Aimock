c71802da477348b29c80c0ecc5374d04168ab146
Koyyalagadda Venkata Rama Raju <ramarajukoyyalagadda@gmail.com>
2024-12-16 20:25:10 +0530

    intial commit

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Navbar from '../../components/Navbar/Navbar'
 
 
 function DashBoard() {
   return (
     <div>
+        <Navbar/>
         <Sidebar/>
       </div>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Sidebar from '../../components/Sidebar/Sidebar'
 import "./Home.css"
 import Footer from '../../components/Footer/footer'
 import { useNavigate } from 'react-router-dom';
+import Navbar from '../../components/Navbar/Navbar'
 
 const Home = () => {
   const jwtToken = Cookies.get('jwt_token')
@@ -24,7 +25,8 @@ const Home = () => {
     <div className="home">
      
       <div>
-        <Sidebar/>
+        {/* <Sidebar/> */}
+        <Navbar/>
         </div>
         <div>
         <div className="home-container">
@@ -42,35 +44,24 @@ const Home = () => {
           feedback and comprehensive mock tests. Upskill, stand out, and secure
           your dream job!"
         </p>
-        <p className="home-description"> 
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-          Eos veritatis cumque suscipit odio dolorum perspiciatis esse rem aliquid
-           non aspernatur sequi exercitationem nostrum maiores accusantium, 
-           officia maxime necessitatibus incidunt molestias illo modi aut commodi,
-            dignissimos ex unde? Praesentium, eos? Ducimus quis maiores quidem laborum
-             recusandae impedit perspiciatis molestias cum eum quae tempora sed, id sunt,
-              dolore eaque architecto. Vitae cumque maxime voluptas, nisi nostrum dignissimos! 
-              Facere quae, modi illum cumque ducimus corporis at eaque ullam possimus suscipit 
-              quam quos doloremque nobis reprehenderit voluptate deleniti earum, dolor asperiores qui non!
-               Consequatur vitae, consequuntur libero debitis impedit quo et eaque officiis alias.
-           </p>
+        
            </div>
            <div className='hom-img'>
-           <img
+           {/* <img
         src="https://www.pngmart.com/files/21/Job-Interview-PNG-Picture.png"
         alt="clothes that get you noticed"
         className="home-desktop-img"
-      />
+      /> */}
            </div>
            </div>
         <div className="buttonSet">
           <div >
-            <img src="./mock4[1].jpg" alt="" />
+            <img src="./rb_4415.png" alt="" className='img1' />
             
             <input type="button" className="shop-now-button" value="Mock Interview" onClick={goToMockInt} />
           </div>
           <div>
-                <img src="./mock7.jpg" alt="" />
+                <img src="./rb_5412.png" alt="" className='img2'/>
             
             <input type="button" className="shop-now-button" value="Mock Tests" onClick={goToMockTest} />
             </div>
